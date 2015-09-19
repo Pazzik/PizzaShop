63ee0a0dd655fd317a86adff1a07563fb57ab880
Pazzik <andrej-bulygin@yandex.ru>
2015-09-19 16:56:23 +0300

    Create products migration

--- db/migrate/20150919134032_create_products.rb
+++ db/migrate/20150919134032_create_products.rb
@@ -1,4 +1,16 @@
 class CreateProducts < ActiveRecord::Migration
   def change
+    create_table :products do |t|
+      t.string  :title
+      t.text    :description
+      t.decimal :size
+      t.decimal :price
+      t.boolean :is_spicy
+      t.boolean :is_vegan
+      t.boolean :is_best_offer
+      t.string  :path_to_image
+
+      t.timestamps
+    end
   end
 end
