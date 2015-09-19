7bf741a4ff899355ecb7c4618796661af705c7e2
Pazzik <andrej-bulygin@yandex.ru>
2015-09-19 17:25:28 +0300

    Add products migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919134032) do
+ActiveRecord::Schema.define(version: 20150919140054) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
