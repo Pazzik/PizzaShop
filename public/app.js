1d152226feb256177069203d3f6e25c1519887b9
Pazzik <andrej-bulygin@yandex.ru>
2015-09-26 17:04:16 +0300

    Update orders button

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ function add_to_cart(id){
 	var count = window.localStorage.getItem('product_'+id);
 	window.localStorage.setItem('product_'+id,count*1+1);
     update_orders_input();
+    update_orders_button();
 }
 function cart_get_number_of_items()
 {
@@ -21,6 +22,12 @@ function update_orders_input(){
     var orders = cart_get_orders();
     $('#orders_input').val(orders);
 }
+
+function update_orders_button(){
+    var text = 'Cart(' + cart_get_number_of_items()+')';
+    $('#orders_button').val(text);
+}
+
 function cart_get_orders()
 {
     var orders = '';
@@ -29,10 +36,8 @@ function cart_get_orders()
         var key = window.localStorage.key(i);
         var value = window.localStorage.getItem(key);
         if (key.indexOf('product_') == 0){
-
             orders+=key+'='+value+',';
         }
     }
-
     return orders;
 }
