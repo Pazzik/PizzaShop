d795ec5652c27992f3e43e025bfdc49d18b79fa4
Pazzik <andrej-bulygin@yandex.ru>
2015-09-26 15:55:33 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -2,3 +2,17 @@ function add_to_cart(id){
 	var count = window.localStorage.getItem('product_'+id);
 	window.localStorage.setItem('product_'+id,count*1+1);
 }
+function cart_get_number_of_items()
+{
+    var cnt = 0;
+    for(var i=0;i < window.localStorage.length;i++)
+    {
+        var key = window.localStorage.key(i);
+        if (key.indexOf('product_') == 0){
+            var value = window.localStorage.getItem(key);
+            cnt+=value*1 +1;
+        }
+    }
+
+    return cnt;
+}
